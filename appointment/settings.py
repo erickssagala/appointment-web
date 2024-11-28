9141f6a57147c3f8dc4ffd8b4a21f39e4aed9ca1
erickssagala <69842748+erickssagala@users.noreply.github.com>
2024-11-28 23:57:50 +0300

    third commit

--- appointment/settings.py
+++ appointment/settings.py
@@ -83,7 +83,8 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_USER_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST')
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT')
     }
 }
 
