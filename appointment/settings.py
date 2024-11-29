bd2c097ef21c1cd2259677cf535d84f0e47678ff
erickssagala <69842748+erickssagala@users.noreply.github.com>
2024-11-29 11:48:10 +0300

    new commit

--- appointment/settings.py
+++ appointment/settings.py
@@ -86,7 +86,6 @@ DATABASES = {
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_USER_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
-        'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
