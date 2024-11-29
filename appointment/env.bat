c251b227dd32ff066e28117b64f18d5b1738fcdb
erickssagala <69842748+erickssagala@users.noreply.github.com>
2024-11-29 11:56:03 +0300

    new commit

--- appointment/env.bat
+++ appointment/env.bat
@@ -3,4 +3,4 @@ SET DB_USER=postgres
 SET DB_USER_PASSWORD=postgres
 SET DB_HOST=localhost
 SET DB_PORT=5432
-SET DATABASE_URL=postgresql://appointmentdb_eoli_user:SMQR1tNhKYj4AaxBJTXz7S7d7r3OVzju@dpg-ct4dg33tq21c73934kug-a/appointmentdb_eoli
+@REM SET DATABASE_URL=postgresql://appointmentdb_eoli_user:SMQR1tNhKYj4AaxBJTXz7S7d7r3OVzju@dpg-ct4dg33tq21c73934kug-a/appointmentdb_eoli
