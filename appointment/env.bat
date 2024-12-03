86023822e0e334cdc35b9ee5551458b51121e1a7
erickssagala <69842748+erickssagala@users.noreply.github.com>
2024-12-03 19:36:45 +0300

    latest1

--- appointment/env.bat
+++ appointment/env.bat
@@ -1,13 +1,24 @@
-SET DB_NAME=appointment
-SET DB_USER=postgres
-SET DB_USER_PASSWORD=postgres
-SET DB_HOST=localhost
+@REM Development
+@REM SET DB_NAME=appointment
+@REM SET DB_USER=postgres
+@REM SET DB_USER_PASSWORD=postgres
+@REM SET DB_HOST=localhost
+@REM SET DB_PORT=5432
+@REM SET DATABASE_URL=postgresql://DB_USER:DB_USER_PASSWORD@localhost:5432/DB_NAME?sslmode=disable
+
+@REM Production
+SET DB_NAME=appointmentdb
+SET DB_USER=appointmentdb_eoli_user
+SET DB_USER_PASSWORD=SMQR1tNhKYj4AaxBJTXz7S7d7r3OVzju
+SET DB_HOST=dpg-ct4dg33tq21c73934kug-a
+SET DB_PORT=5432
+SET DATABASE_URL=postgresql://appointmentdb_eoli_user:SMQR1tNhKYj4AaxBJTXz7S7d7r3OVzju@dpg-ct4dg33tq21c73934kug-a/appointmentdb_eoli
 
 SET EMAIL_HOST=smtp.gmail.com
 SET EMAIL_HOST_USER=ericknabstar@gmail.com
 SET DEFAULT_FROM_EMAIL=ericknabstar@gmail.com
 SET EMAIL_HOST_PASSWORD=azcwouvktbqpbfro
 SET EMAIL_BACKEND=django.core.mail.backends.smtp.EmailBackend
-SET DB_PORT=5432
-@REM SET DATABASE_URL=postgresql://DB_USER:DB_USER_PASSWORD@localhost:5432/DB_NAME?sslmode=disable
+
+
 
