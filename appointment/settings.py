3d27f211eecfe695169f562298e0c151d78eee1a
erickssagala <69842748+erickssagala@users.noreply.github.com>
2024-12-05 10:57:40 +0300

    latest3

--- appointment/settings.py
+++ appointment/settings.py
@@ -41,7 +41,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'appointment.middleware.DisableCacheMiddleware'
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
